e64189520fdbb400dfda0a84c348d9c07f925632
Dalvin Josias Sejour <dalvin.sejour@dutchie.com>
2023-07-17 00:18:22 -0700

    add some features

--- player.gd
+++ player.gd
@@ -22,6 +22,12 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 	
 	position += velocity * delta
 	position.x =  clamp(position.x, 0, screen_size.x)
